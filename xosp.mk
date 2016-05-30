dc67d821ed67a20c2730ae3bb92294113657dd97
Vatsal <koradiavatsal@gmail.com>
2016-05-30 19:24:24 +0530

    XOSP'fy

--- xosp.mk
+++ xosp.mk
@@ -14,13 +14,16 @@
 
 $(call inherit-product, device/motorola/osprey/full_osprey.mk)
 
+$(call inherit-product, vendor/xosp/config/xosp.mk)
+
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
 TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := cm_osprey
+PRODUCT_NAME := xosp_osprey
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
+RELEASE_TYPE := XOSP_OFFICIAL
