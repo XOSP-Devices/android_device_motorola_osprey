c039aa59b3c9fbad387735d6ca74a92a65c5585e
Esrom Cataneo <esromgalaxy5@gmail.com>
2017-04-26 14:13:10 -0300

    osprey: update dependencies and xosp.mk

--- xosp.mk
+++ xosp.mk
@@ -25,4 +25,9 @@ PRODUCT_NAME := xosp_osprey
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := osprey
+
+# Root method
+ROOT_METHOD := magisk
+
+# XOSP release typed
 RELEASE_TYPE := XOSP_OFFICIAL
