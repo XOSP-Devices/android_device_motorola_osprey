715e0d8552bd3f99f0bd07c6b33cdeb7f3b1064f
Esrom Cataneo <esromgalaxy5@gmail.com>
2017-05-06 11:26:02 -0300

    osprey: set selinux permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_osprey
 
 # Kernel
 TARGET_KERNEL_CONFIG := osprey_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216     # 16384 * 1024 mmcblk0p31
